594b90ee4cd86b86e6f094523a5c08650933ff61
Nader Kanounji <naderkanounji@gmail.com>
2022-04-02 15:39:33 +0300

    import Roboto font

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -3,6 +3,16 @@ import 'styled-components'
 
 declare module 'styled-components' {
   export interface DefaultTheme {
+    bodyFontSize: string
+    fonts: {
+      primary: string
+    }
+    fontWeights: {
+      light: number
+      regular: number
+      medium: number
+      bold: number
+    }
     colors: {
       lightGrey: string
       [key: string]: string
