cae8465a73c3deabf044c5ef6d2e4720caad11cc
Nader Kanounji <naderkanounji@gmail.com>
2022-04-02 15:39:33 +0300

    import Roboto font

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -3,6 +3,15 @@ import 'styled-components'
 
 declare module 'styled-components' {
   export interface DefaultTheme {
+    fonts: {
+      primary: string
+    }
+    fontWeights: {
+      light: number
+      regular: number
+      medium: number
+      bold: number
+    }
     colors: {
       lightGrey: string
       [key: string]: string
--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,6 +1,15 @@
 import { DefaultTheme } from 'styled-components'
 
 export const defaultTheme: DefaultTheme = {
+  fonts: {
+    primary: 'Roboto',
+  },
+  fontWeights: {
+    light: 300,
+    regular: 400,
+    medium: 500,
+    bold: 700,
+  },
   colors: {
     lightGrey: 'lightgrey',
   },
